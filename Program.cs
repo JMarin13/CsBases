8d11683fab0a0f178da5dfda6d429dcdcbf41fe9
jmarin13 <jmarintobar@gmail.com>
2025-08-19 16:18:44 -0500

    Variables, data types

--- Program.cs
+++ Program.cs
@@ -2,6 +2,22 @@
 {
     static void Main()
     {
-        WriteLine("Hello, World!");
+        /*
+         * Data types
+        */
+
+        // Explicit types
+        string description = "Rice";
+        int quantity = 2;
+        double price = 5500;
+
+        WriteLine($"Product: {description} \nQuantity: {quantity} \nPrice: ${price}");
+
+        // Var types
+        var name = "Johny Marin";
+        var age = 26;
+        var isHappy = true;
+
+        WriteLine($"\nName: {name} \nAge: {age} \nIs Happy: {isHappy}");
     }
 }
