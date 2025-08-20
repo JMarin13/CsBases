7fa8a03d6984b8310847184746885d45d24d398e
jmarin13 <jmarintobar@gmail.com>
2025-08-20 14:45:36 -0500

    Add Product class and IProduct interface

---
+++ fundamentals/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsBases.fundamentals
+{
+    public class Product : IProduct
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public double Price { get; set; }
+        public bool IsAvailable { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public Guid UniqueCode { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+            CreatedAt = DateTime.Now;
+            UniqueCode = Guid.NewGuid();
+        }
+
+        public void ApplyDiscount(double percentaje)
+        {
+            var discountAmount = Price * (percentaje / 100);
+            Price -= discountAmount;
+        }
+
+        public string GetDescription()
+        {
+            return $"{Name} - ${Price}";
+        }
+    }
+}
