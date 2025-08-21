fd1d4efaebbcbde89603b6f15be08585da9b11bf
jmarin13 <jmarintobar@gmail.com>
2025-08-21 15:45:25 -0500

    Operation of the adapter pattern

---
+++ fundamentals/03-AdapterPattern/ProductAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsBases.fundamentals._03_AdapterPattern
+{
+    public class ProductAdapter
+    {
+        public static ProductDto ToDto(Product product)
+        {
+            ProductDto productDto = new ProductDto
+            {
+                Name = product.Name ?? "Without name",
+                Price = product.Price,
+                Code = $"{product.Id}-{product.UniqueCode.ToString().Substring(0, 8)}"
+            };
+
+            return productDto;
+        }
+    }
+}
