6afa89fb5caad6ba9226a0db7a28e50b34f77ef5
jmarin13 <jmarintobar@gmail.com>
2025-08-21 12:27:31 -0500

    The code is separated and organized into folders

--- Program.cs
+++ Program.cs
@@ -4,26 +4,6 @@ class Program
 {
     static void Main()
     {
-        /*
-         * Data types
-        */
-
-        // Explicit types
-        string description = "Rice";
-        int quantity = 2;
-        double price = 5500;
-
-        WriteLine($"Product: {description} \nQuantity: {quantity} \nPrice: ${price}");
-
-        // Var types
-        var name = "Johny Marin";
-        var age = 26;
-        var isHappy = true;
-
-        WriteLine($"\nName: {name} \nAge: {age} \nIs Happy: {isHappy}");
-
-        WriteLine();
-
         // Creating Product Object
         var laptop = new Product("Asus TUF Gaming A15", 3500000);
         WriteLine(laptop.GetDescription());
