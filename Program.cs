ea00b08c2bc28530f7ad25b7773f79a9e92d70c4
jmarin13 <jmarintobar@gmail.com>
2025-08-21 15:46:17 -0500

    How dependency injection works

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using CsBases.fundamentals;
+using CsBases.fundamentals._03_AdapterPattern;
+using CsBases.fundamentals._04_DependencyInjection;
 
 class Program
 {
@@ -11,5 +13,21 @@ class Program
         // Creating ServiceProduct Object
         var support = new ServiceProduct("Technical Support", 50000, 30);
         WriteLine(support.GetDescription());
+
+        // Creating Product Object (Adapter Pattern)
+        var mouse = new Product("Mouse Gamer", 200000);
+        var mouseDto = ProductAdapter.ToDto(mouse);
+        WriteLine($"\n{mouseDto.Name} - ${mouseDto.Price} - Code: {mouseDto.Code}");
+
+        // dependency injection
+        ILabelService labelService = new LabelService();
+        var managerMonitor = new ProductManager(labelService);
+
+        var monitor = new Product("Samsung Monitor 32'", 1000000);
+        var instalation = new ServiceProduct("Monitor Instalation", 56000, 30);
+
+        managerMonitor.PrintLabel(monitor);
+        managerMonitor.PrintLabel(instalation);
+
     }
 }
