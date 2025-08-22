dac09aec2366727bf0750f2b62e645d2a3680174
jmarin13 <jmarintobar@gmail.com>
2025-08-22 09:37:44 -0500

    How Async methods work

---
+++ fundamentals/05-AsyncMethods/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsBases.fundamentals._05_AsyncMethods
+{
+    public class ProductRepository
+    {
+        public Product GetProduct(int id)
+        {
+            Product product = new Product("Simulated Product", 250000);
+
+            return product;
+        }
+
+        public async Task<Product> GetProductAsync(int id)
+        {
+            WriteLine("\nSearching product...");
+            await Task.Delay(2000);
+
+            return new Product("Simulated product 2", 255000);
+        }
+    }
+}
